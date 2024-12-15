ffdcfea5fb9b2350d711b88b876547b601781982
chethan <chethanchethu99339@gmail.com>
2024-12-15 21:30:34 +0530

    added the dom selection

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,17 @@
 export let locations = document.querySelector('#location')
 
+export let DOMElements = {
+    city: document.querySelector("#city"),
+    country: document.querySelector("#country"),
+    day: document.querySelector("#day"),
+    date: document.querySelector("#date"),
+    temperature: document.querySelector("#temperature"),
+    weatherIcon: document.querySelector("#weatherIcon"),
+    weatherDesc: document.querySelector("#weatherDesc"),
+    precipitationValue: document.querySelector("#precipitationValue"),
+    humidityValue: document.querySelector("#humidityValue"),
+    windValue: document.querySelector("#windValue"),
+}
+
+
 
